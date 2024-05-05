be0a974a110390ebc4d1e39af0db85d0de9635ae
manant0rn <m_manantorn@hotmail.com>
2024-05-05 13:52:05 +0700

    learn basic-vuejs3.x v.1.2 create compunent

---
+++ src/components/FormComponent.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>{{ message }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"FormComponent",
+    data(){
+        return{
+            message:"แบบฟอร์มสำหรับข้อมูลพรนักงาน"
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/ListData.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+    <Person/>
+  </div>
+</template>
+
+<script>
+import Person from './Person.vue'
+export default {
+    name:"ListData",
+    components:{
+      Person
+    },
+    data(){
+        return{
+            message:"แสดงรายชื่อพนักงานทุกคน"
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
